2b975515531bfa7c010ea4b8a9915d671d385a2f
IlyaBelik <37375602+IlyaBelik@users.noreply.github.com>
2021-01-18 19:21:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 const cors = require('cors')
+const keys = require('./configs/keys')
 
-mongoose.connect("mongodb+srv://test-user:fwge4xeDuG23WtN6@testappcluster.nymyb.mongodb.net/testdb?retryWrites=true&w=majority")
+mongoose.connect(keys.mongo_uri)
 
 const app = express()
 app.use(bodyparser.json())
 app.use(cors())
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client-side/build"))
+    const path = require('path')
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client-side", "build", "index.html"))
+    })
+}
 
 app.use('/announcements', require('./routes/announcement') )
 
